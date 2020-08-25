f7cd29223ce42a28cef4493a84f5f389f5ebe3e8
Charan Kannati <54478438+Charansan2014@users.noreply.github.com>
2020-08-25 14:54:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
-
+var multer = require('multer');
+var upload = multer();
 // require and use "multer"...
 
 var app = express();
@@ -18,6 +19,18 @@ app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
 
+app.post('/api/fileanalyse',upload.single('upfile'), function(req, res, next){
+    var file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+ res.json({"name":file.originalname,"type":file.mimetype,"size":file.size})
+})
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
+
+//final response must be a json with name, type and size of the uploaded file
